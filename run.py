f23ee743889c776b05988fd1fe5cd448c2ba4346
Tristan Lecuyer <tristan.use@gmail.com>
2021-03-08 00:24:58 +0100

    Add Logger

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 from grandpy.response import Response
+import os
+import logging 
 
 app = Flask(__name__)
+
+logger = logging.getLogger()
+
+formatter = logging.Formatter('%(process)d %(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
+
+logger.info(os.environ)
+
+
 
 @app.route('/')
 def index():
